88ac750c2f5cdc95b15ef54011c5b0e302af6fea
Cipher <itayleybovich@gmail.com>
2023-08-20 10:35:40 +0300

    svgr config to remove prefix from svg classnames

--- src/icons.tsx
+++ src/icons.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import {Gear, Gears} from './icon-components';
 
+type CosmoIconSupplier = (props:React.HTMLProps<HTMLDivElement>) => React.JSX.Element;
+
 type CosmoIcon = {
-	solid: (props:React.HTMLProps<HTMLDivElement>) => React.JSX.Element;
+	solid: CosmoIconSupplier;
+	stroke?: CosmoIconSupplier;
 }
 
-const cosmoIcon = (Icon:React.ElementType) => (props:React.HTMLProps<HTMLDivElement>) => {
+const cosmoIcon = (Icon:React.ElementType): CosmoIconSupplier => (props) => {
 	const {className,...rest} = props;
 	const _className = `cosmo-icon ${className ?? ''}`.trim();
 	return <div className={_className} {...rest}><Icon/></div>
--- svgr.config.js
+++ svgr.config.js
@@ -3,4 +3,7 @@ const path = require('path');
 module.exports = {
 	typescript:true,
 	outDir: path.join(__dirname,'/src/icon-components'),
+	svgoConfig : {
+		plugins:['preset-default']
+	}
 }
