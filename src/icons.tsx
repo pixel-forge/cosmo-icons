7e67ca55131d2fa6e64d8fd67b6da085199118aa
Cipher <itayleybovich@gmail.com>
2023-08-24 01:33:28 +0300

    Added icons, added outline versions of icons

--- src/icons.tsx
+++ src/icons.tsx
@@ -1,20 +1,22 @@
 import * as React from 'react';
-import {Gear, Gears} from './icon-components';
+import {EmailOutline, EmailSolid, GearOutline, GearSolid, GearsOutline, GearsSolid, SearchOutline, SearchSolid} from './icon-components';
 
-type CosmoIconSupplier = (props:React.HTMLProps<HTMLDivElement>) => React.JSX.Element;
+type CosmoIconSupplier = (props: React.HTMLProps<HTMLDivElement>) => React.JSX.Element;
 
 type CosmoIcon = {
 	solid: CosmoIconSupplier;
-	stroke?: CosmoIconSupplier;
+	outline?: CosmoIconSupplier;
 }
 
-const cosmoIcon = (Icon:React.ElementType): CosmoIconSupplier => (props) => {
-	const {className,...rest} = props;
+const cosmoIcon = (Icon: React.ElementType): CosmoIconSupplier => (props) => {
+	const {className, ...rest} = props;
 	const _className = `cosmo-icon ${className ?? ''}`.trim();
-	return <div className={_className} {...rest}><Icon/></div>
-}
+	return <div className={_className} {...rest}><Icon/></div>;
+};
 
-export const CosmoIcons:{[K:string]: CosmoIcon} = {
-	gear: {solid: cosmoIcon(Gear)},
-	gears: {solid: cosmoIcon(Gears)},
-}
+export const CosmoIcons: { [K: string]: CosmoIcon } = {
+	email: {solid:cosmoIcon(EmailSolid),outline:cosmoIcon(EmailOutline)},
+	gear: {solid: cosmoIcon(GearSolid), outline: cosmoIcon(GearOutline)},
+	gears: {solid: cosmoIcon(GearsSolid), outline: cosmoIcon(GearsOutline)},
+	search: {solid:cosmoIcon(SearchSolid),outline:cosmoIcon(SearchOutline)},
+};
