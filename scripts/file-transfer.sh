c0adcb1a21dd48d0d2712567dab21d996ee8487f
Cipher <itayleybovich@gmail.com>
2023-11-14 00:36:25 +0200

    default icon design added

--- scripts/file-transfer.sh
+++ scripts/file-transfer.sh
@@ -1,6 +1,3 @@
-_cd "$(pwd)/src"
-  find . -name '*.svg' | cpio -pdm "$(pwd)/dist" > /dev/null
-  find . -name '*.png' | cpio -pdm "$(pwd)/dist" > /dev/null
-  find . -name '*.jpg' | cpio -pdm "$(pwd)/dist" > /dev/null
-  find . -name '*.jpeg' | cpio -pdm "$(pwd)/dist" > /dev/null
-_cd-
+#!/bin/bash
+
+rsync -av --include='*/' --include='*.scss' --exclude='*' "$(pwd)/src/" "$(pwd)/dist/" > /dev/null
